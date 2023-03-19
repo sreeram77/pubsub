b78da25c84f5777dab37cf32041157540e5ce959
Sreeram Narayanan <sreeram.an1994@gmail.com>
2023-03-19 23:11:15 +0530

    add: generated client and server implementation from proto

--- event/event_test.go
+++ event/event_test.go
@@ -2,15 +2,13 @@ package event
 
 import (
 	"testing"
-
-	"golang.org/x/text/message"
 )
 
 func TestNew(t *testing.T) {
 	topic := "Test"
 	message := "test subject"
 	body := []byte(message)
-	
+
 	e := New(topic, body)
 
 	if string(e.body) != message || e.topic != topic {
