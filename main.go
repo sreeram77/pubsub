a427cf135e7bfd10930f46663cf611e0bc5495bb
Sreeram Narayanan <sreeram.an1994@gmail.com>
2023-03-19 23:11:53 +0530

    add: init grpc server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"net"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/sreeram77/pubsub/publisher"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("Hello!")
+	log.SetFormatter(&log.TextFormatter{})
+
+	lis, err := net.Listen("tcp", ":8000")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Debug("Listening for Registration...")
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	publisher.RegisterPublisherServer(grpcServer, publisher.NewServer())
+
+	grpcServer.Serve(lis)
 }
